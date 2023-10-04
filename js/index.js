efc9ff1470fc5d9baa76460b59c8a1fc9b224c1b
fajrulkamal <fajrulkamal.fk2@gmail.com>
2023-10-04 19:30:12 +0700

    refactor index.js to better organize the code

--- js/index.js
+++ js/index.js
@@ -1,68 +1,62 @@
-
-let backToTopBtn = document.querySelector('.back-to-top')
-
-window.onscroll = () => {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-        backToTopBtn.style.display = 'flex'
-    } else {
-        backToTopBtn.style.display = 'none'
-    }
+// Back to top button functionality
+const backToTopFunctionality = () => {
+    const backToTopBtn = document.querySelector('.back-to-top');
+    window.onscroll = () => {
+        backToTopBtn.style.display = (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) ? 'flex' : 'none';
+    };
 }
 
-
-let menuItems = document.getElementsByClassName('menu-item')
-
-Array.from(menuItems).forEach((item, index) => {
-    item.onclick = (e) => {
-        let currMenu = document.querySelector('.menu-item.active')
-        currMenu.classList.remove('active')
-        item.classList.add('active')
-    }
-})
-
-
-
-let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
-
-let elToShow = document.querySelectorAll('.animasi-scroll')
-
-isElInViewPort = (el) => {
-    let rect = el.getBoundingClientRect()
-
-    return (
-        (rect.top <= 0 && rect.bottom >= 0)
-        ||
-        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
-        ||
-        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
-    )
+// Menu items functionality
+const menuItemsFunctionality = () => {
+    const menuItems = document.getElementsByClassName('menu-item');
+    Array.from(menuItems).forEach(item => {
+        item.onclick = () => {
+            document.querySelector('.menu-item.active').classList.remove('active');
+            item.classList.add('active');
+        };
+    });
 }
 
-loop = () => {
-    elToShow.forEach((item, index) => {
-        if (isElInViewPort(item)) {
-            item.classList.add('start')
-        } else {
-            item.classList.remove('start')
-        }
-    })
+// Scroll animation functionality
+const scrollAnimationFunctionality = () => {
+    const scroll = window.requestAnimationFrame || (callback => window.setTimeout(callback, 1000/60));
+    const elToShow = document.querySelectorAll('.animasi-scroll');
 
-    scroll(loop)
+    const isElInViewPort = el => {
+        const rect = el.getBoundingClientRect();
+        return (
+            (rect.top <= 0 && rect.bottom >= 0) ||
+            (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+            (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+        );
+    }
+
+    const loop = () => {
+        elToShow.forEach(item => {
+            item.classList[isElInViewPort(item) ? 'add' : 'remove']('start');
+        });
+        scroll(loop);
+    }
+
+    loop();
 }
 
-loop()
+// Bottom navigation functionality
+const bottomNavFunctionality = () => {
+    const bottomNavItems = document.querySelectorAll('.mobile-navigasi-item');
+    const bottomMove = document.querySelector('.mobile-navigasi-animasipindah');
 
+    bottomNavItems.forEach((item, index) => {
+        item.onclick = () => {
+            document.querySelector('.mobile-navigasi-item.active').classList.remove('active');
+            item.classList.add('active');
+            bottomMove.style.left = `${index * 25}%`;
+        };
+    });
+}
 
-let bottomNavItems = document.querySelectorAll('.mobile-navigasi-item')
-
-let bottomMove = document.querySelector('.mobile-navigasi-animasipindah')
-
-bottomNavItems.forEach((item, index) => {
-    item.onclick = (e) => {
-        console.log('object')
-        let crrItem = document.querySelector('.mobile-navigasi-item.active')
-        crrItem.classList.remove('active')
-        item.classList.add('active')
-        bottomMove.style.left = index * 25 + '%'
-    }
-})
+// Initialize all functionalities
+backToTopFunctionality();
+menuItemsFunctionality();
+scrollAnimationFunctionality();
+bottomNavFunctionality();
